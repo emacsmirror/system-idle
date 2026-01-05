b5a6cc04af616b5972ac030943a3ed367bee9cdc
Martin Edström <meedstrom@runbox.eu>
2026-01-05 20:25:29 +0100

    Refactor

--- sys-idle.el
+++ sys-idle.el
@@ -40,14 +40,14 @@
 (declare-function dbus-get-property "dbus" (bus service path interface property))
 (require 'seq)
 
-(defun sys-idle-seconds--poll-mac ()
+(defun sys-idle--poll-mac ()
   "Copy of `org-mac-idle-seconds'."
   (string-to-number
    (shell-command-to-string
     "ioreg -c IOHIDSystem | perl -ane 'if (/Idle/) {$idle=(pop @F)/1000000000; print $idle; last}'")))
 
 (defvar sys-idle--x11-program nil)
-(defun sys-idle-seconds--poll-x11 ()
+(defun sys-idle--poll-x11 ()
   "Like `org-x11-idle-seconds'."
   (unless sys-idle--x11-program
     (error "sys-idle-seconds-x11: Install x11idle or xprintidle"))
@@ -61,7 +61,7 @@
 (defvar sys-idle--dbus-session-path nil
   "Copy of `org-logind-dbus-session-path'.")
 
-(defun sys-idle-seconds--poll-elogind ()
+(defun sys-idle--poll-elogind ()
   "Copy of `org-logind-user-idle-seconds'."
   (unless (fboundp 'dbus-get-property)
     (require 'dbus))
@@ -74,7 +74,7 @@
         1e6)))
 
 ;; https://unix.stackexchange.com/questions/396911/how-can-i-tell-if-a-user-is-idle-in-wayland
-(defun sys-idle-seconds--poll-gnome ()
+(defun sys-idle--poll-gnome ()
   "Check Mutter's idea of idle time, even on Wayland."
   (let* ((output (with-temp-buffer
                    (call-process "dbus-send" nil (current-buffer) nil
@@ -85,14 +85,14 @@
                    (buffer-string)))
          (idle-ms (if (string-match (rx space (+ digit) eol) output)
                       (string-to-number (match-string 0 output))
-                    (error "Function `sys-idle-seconds--poll-gnome' not working"))))
+                    (error "Function `sys-idle--poll-gnome' not working"))))
     (/ idle-ms 1000.0)))
 
 ;; https://github.com/swaywm/swayidle/issues/147
 ;; https://github.com/swaywm/swayidle/issues/181
 ;; https://github.com/marvin1099/wayidletool
 (defvar sys-idle--swayidle-process nil)
-(defun sys-idle-seconds--poll-swayidle ()
+(defun sys-idle--poll-swayidle ()
   "Check idle on compositors supporting the ext-idle-notify protocol.
 This includes KDE Plasma and Sway.
 Returns 0 if invoked during the first 9 seconds."
@@ -111,7 +111,8 @@ Returns 0 if invoked during the first 9 seconds."
 
 (defvar sys-idle-seconds-function nil
   "Function invoked by `sys-idle-seconds', must return a number.
-You can manually set this to `sys-idle-seconds-fallback'
+
+You can set this to `sys-idle-seconds-fallback'
 if you know what you are doing.")
 
 (defun sys-idle-seconds ()
@@ -123,41 +124,52 @@ value of `sys-idle-seconds-function' does this correctly.
 
 Always returns a number."
   (unless sys-idle-seconds-function
-    ;; Guess which function is most appropriate on this system.
-    (setq sys-idle-seconds-function
-          (or (and (eq system-type 'darwin)
-                   #'sys-idle-seconds--poll-mac)
-              (let ((DESKTOP_SESSION (getenv "DESKTOP_SESSION")))
-                (and DESKTOP_SESSION
-                     (not (string-search "xorg" DESKTOP_SESSION))
-                     (if (string-match-p (rx (or "gnome" "ubuntu")) DESKTOP_SESSION)
-                         #'sys-idle-seconds--poll-gnome
-                       (and (string-match-p (rx "plasma") DESKTOP_SESSION) ;; KDE
-                            (if (executable-find "swayidle")
-                                #'sys-idle-seconds--poll-swayidle
-                              (error "sys-idle-seconds: Install swayidle"))))))
-              (and (setq sys-idle--dbus-session-path
-                         (when (and (boundp 'dbus-runtime-version)
-                                    (require 'dbus nil t)
-                                    (member "org.freedesktop.login1" (dbus-list-activatable-names)))
-                           (ignore-errors
-                             (dbus-call-method
-                              :system "org.freedesktop.login1"
+    (sys-idle-recalculate-variables t))
+  (let ((value (funcall sys-idle-seconds-function)))
+    (if (numberp value)
+        value
+      (error "Function at sys-idle-seconds-function did not return a number"))))
+
+(defun sys-idle-recalculate-variables (&optional assert)
+  "Try to set `sys-idle-seconds-function', emitting errors if ASSERT."
+  (setq sys-idle--x11-program
+        (seq-find #'executable-find '("x11idle" "xprintidle")))
+  (setq sys-idle--dbus-session-path
+        (when (and (boundp 'dbus-runtime-version)
+                   (require 'dbus nil t)
+                   (member "org.freedesktop.login1" (dbus-list-activatable-names)))
+          (ignore-errors
+            (dbus-call-method :system
+                              "org.freedesktop.login1"
                               "/org/freedesktop/login1"
                               "org.freedesktop.login1.Manager"
-                              "GetSessionByPID" (emacs-pid)))))
-                   #'sys-idle-seconds--poll-elogind)
-              ;; NOTE: This condition is also true under XWayland, so it must come
-              ;; after all other checks for Wayland compositors if we want it to be
-              ;; invoked under "true" X only.
-              (and (eq window-system 'x)
-                   (or (setq sys-idle--x11-program
-                             (seq-find #'executable-find '("x11idle" "xprintidle")))
-                       (error "sys-idle-seconds: Install x11idle or xprintidle"))
-                   #'sys-idle-seconds--poll-x11)
-              (error "sys-idle-seconds: Could not get idle time on this system"))))
-  (or (funcall sys-idle-seconds-function)
-      (error "Function at sys-idle-seconds-function did not return a number")))
+                              "GetSessionByPID"
+                              (emacs-pid)))))
+  (setq sys-idle-seconds-function
+        (or (and (eq system-type 'darwin)
+                 #'sys-idle--poll-mac)
+            (let ((DESKTOP_SESSION (getenv "DESKTOP_SESSION")))
+              (and DESKTOP_SESSION
+                   (not (string-search "xorg" DESKTOP_SESSION))
+                   (if (string-match-p (rx (or "gnome" "ubuntu")) DESKTOP_SESSION)
+                       #'sys-idle--poll-gnome
+                     (and (string-match-p (rx "plasma") DESKTOP_SESSION) ;; KDE
+                          (if (executable-find "swayidle")
+                              #'sys-idle--poll-swayidle
+                            (when assert
+                              (error "sys-idle: Install swayidle")))))))
+            (and sys-idle--dbus-session-path
+                 #'sys-idle--poll-elogind)
+            ;; NOTE: This condition is also true under XWayland, so it must come
+            ;; after all other checks for Wayland compositors if we want it to be
+            ;; invoked under true X only.
+            (and (eq window-system 'x)
+                 (if sys-idle--x11-program
+                     #'sys-idle--poll-x11
+                   (when assert
+                     (error "sys-idle: Install x11idle or xprintidle"))))
+            (when assert
+              (error "sys-idle: Could not get idle time on this system")))))
 
 (defun sys-idle-seconds-fallback ()
   "Copy of `org-emacs-idle-seconds'.
